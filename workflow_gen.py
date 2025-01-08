033dac35d00118c5e6e73ed879594991e5d88283
Yichuan Deng <ycdeng@cs.washington.edu>
2025-01-07 21:16:13 -0800

    fix issue

--- workflow_gen.py
+++ workflow_gen.py
@@ -151,7 +151,7 @@ class ExperimentWrapper():
                     print(f'------\nQuery {idx}: {query}')    
 
                 for turn in range(self.args.max_turns):
-                    self.construct_policy_dialogue([init_observation] + turn_history["best_observations"], turn_history["best_actions"], args.models[turn])
+                    self.construct_policy_dialogue([init_observation] + turn_history["best_observations"], turn_history["best_actions"], args.models[turn], turn)
                     actions = []
                     try:
                         while len(actions) == 0:
